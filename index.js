4d27e5f39fa375b11fca3e15ed2e83994ae13f97
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-02 18:03:51 +0600

    single product api done

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ async function run(){
             const query = {_id: ObjectId(id)};
             const result = await productCollection.deleteOne(query);
             res.send(result);
+        });
+        // single product api 
+        app.get('/product/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
         })
     }
     finally{
