3429141cb25e47811e13474d5174dcee179ffcf7
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-02 21:00:19 +0600

    count api added

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1viv4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const productCollection = client.db("carManager").collection("product");
-        app.get('/product', async(req,res)=>{
-            const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
-        // delete 
-        app.delete('/product/:id', async(req, res)=>{
-            const id = req.params.id;
-            console.log(id);
-            const query = {_id: ObjectId(id)};
-            const result = await productCollection.deleteOne(query);
-            res.send(result);
-        });
-        // single product api 
-        app.get('/product/:id', async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const product = await productCollection.findOne(query);
-            res.send(product);
-        })
-    }
-    finally{
-}
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("carManager").collection("product");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    // delete
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    // single product api
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+    // count api
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      const newQuantity = parseInt(product.quantity) - 1;
+      await productCollection.updateOne(query, {
+        $set: { quantity: newQuantity + "" },
+      });
+      res.send(product);
+    });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Running Car Manager!')
+app.get("/", (req, res) => {
+  res.send("Running Car Manager!");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
